58f1648f6f8f882424dbecfa6abc7022da9b3139
sualeh-farooq <138825396+sufiyan-farooq@users.noreply.github.com>
2023-10-11 20:25:56 +0500

    complete java assignment1

---
+++ app.js
@@ -0,0 +1,144 @@
+// Chapter 1
+
+
+// ALERT
+
+
+alert ("Error! Please enter a valid password");
+alert('Sufiyan Farooq');
+alert(3*5);
+alert('Hello\nCoding');
+
+// Chapter 2
+// Variable
+// 1. Declare a variable called username.
+var name = "username"
+alert (name);
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+var myName = "Java Script";
+alert (myName);
+
+
+
+// Declare a JS variable, titled message.
+var name;
+
+// Assign “Hello World” to variable message
+name = "Hello World";
+
+// Display the message in alert box.
+alert(name);
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+var fullname = "John Deo";
+alert (fullname);
+
+
+var age = "16 Years Old";
+alert(age);
+
+var cource = "Web App And Development";
+alert (cource);
+
+// Write a script to display the following alert using one JS
+variable:
+
+var name = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(name);
+
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+var email = "sufiyanfarooq047@gmail.com";
+alert(email);
+
+
+// Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+var book = "A smart wway to learn JavaScript";
+alert(book);
+
+
+
+var end ='“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+alert(end)      
+
+
+// chapter 3 
+// variable for number
+
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+var age = "i am 20 Years old"
+alert(age)
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+var initialize = 15
+// 'you visited this website 15 Times'
+
+
+alert("You have visited this website" + initialize + "Time")
+
+const msg = `You have visited this website ${initialize} Time`
+console.log(msg)
+console.log("You have visited this website" + initialize + "Time")
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+var birthYear = 2004
+document.write (birthYear)
+
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+var name = 'John Deo ordered 5 T-shirts (s) on XYZ clothing store'
+document.write(name)
+
+// Chapter 4
+// legal or illegal
+
+// 1. Declare 3 variables in one statement.
+
+
+var variable1,variable2,variable3;
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// Legal
+var $name = "Sufiyan"
+var userAge = 20
+var user_Qualification = "Inter"
+var $user_From = "Karachi"
+var user2nd = "Farooq"
+
+//Illegal
+
+// var !name = "Sufiyan"
+// var User-Age = 20
+// var 2nduser = "Farooq"
+// var 12345 = 'number'
+// var UserQualified? = 'inter'
+
+
+
+
+
